0956f03d443c6da6591b4f7007c623aa9c91ba91
theDanteAesthetic <dante.squires@sonomaacademy.org>
2024-02-28 13:34:35 -0800

    Removed js from html, added event handlers
    index.html - removed js from clickme button
    page.js - added page.init for initializing elements, added event handlers for the window

--- page.js
+++ page.js
@@ -5,5 +5,14 @@ const page = {
         const para = document.createElement("p");
         para.innerHTML = "Hello, " + fUsername.value + "!!";
         outputDiv.appendChild(para);
+    },
+
+    init: function(){
+        console.log("page loaded")
+        const fClickMeButton = document.querySelector("#clickMeButton");
+        fClickMeButton.addEventListener("click", page.default);
+        console.log("done initializing")
     }
 }
+
+window.addEventListener("load", page.init);
